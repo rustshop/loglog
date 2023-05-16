def285c92e32ef16775f9bd1ed684f5cee0d5e22
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-05-15 23:18:52 -0700

    refactor: benches

--- loglogd/benches/basic.rs
+++ loglogd/benches/basic.rs
@@ -2,40 +2,57 @@
 mod common;
 
 use common::TestLoglogd;
-use criterion::{criterion_group, criterion_main, Criterion};
+use convi::ExpectFrom;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use loglog::Client;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let server = TestLoglogd::new().unwrap();
 
-    let small_entry = &[1u8, 2u8, 3u8];
-    let big_entry = &[1u8; 10000];
+    let entries = [[1u8, 2u8, 3u8].as_slice(), [1u8; 10000].as_slice()];
 
     let mut client = server.new_client().unwrap();
-
-    c.bench_function("small write nocommit", |b| {
-        b.iter(|| client.append_nocommit(small_entry))
-    });
-    c.bench_function("big write nocommit", |b| {
-        b.iter(|| client.append_nocommit(big_entry))
-    });
-    c.bench_function("small write commit", |b| {
-        b.iter(|| client.append(small_entry))
-    });
-    c.bench_function("big write commit", |b| b.iter(|| client.append(big_entry)));
-
-    c.bench_function("small roundtrip", |b| {
-        b.iter(|| {
-            client.append_nocommit(small_entry).unwrap();
-            client.read().unwrap();
-        })
-    });
-    c.bench_function("big roundtrip", |b| {
-        b.iter(|| {
-            client.append_nocommit(big_entry).unwrap();
-            client.read().unwrap();
-        })
-    });
+    {
+        let mut write_nocommit = c.benchmark_group("write nocommit");
+        for entry in entries {
+            write_nocommit.throughput(criterion::Throughput::Bytes(u64::expect_from(entry.len())));
+            write_nocommit.bench_with_input(
+                BenchmarkId::from_parameter(entry.len()),
+                entry,
+                |b, entry| b.iter(|| client.append_nocommit(entry)),
+            );
+        }
+        write_nocommit.finish();
+    }
+    {
+        let mut write_commit = c.benchmark_group("write commit");
+        for entry in entries {
+            write_commit.throughput(criterion::Throughput::Bytes(u64::expect_from(entry.len())));
+            write_commit.bench_with_input(
+                BenchmarkId::from_parameter(entry.len()),
+                entry,
+                |b, entry| b.iter(|| client.append(entry)),
+            );
+        }
+        write_commit.finish();
+    }
+    {
+        let mut roundtrip = c.benchmark_group("roundtrip");
+        for entry in entries {
+            roundtrip.throughput(criterion::Throughput::Bytes(u64::expect_from(entry.len())));
+            roundtrip.bench_with_input(
+                BenchmarkId::from_parameter(entry.len()),
+                entry,
+                |b, entry| {
+                    b.iter(|| {
+                        client.append_nocommit(entry).unwrap();
+                        client.read().unwrap();
+                    })
+                },
+            );
+        }
+        roundtrip.finish();
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
