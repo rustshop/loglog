d1607474a4d55204d6bd9374b616e42b7820a65f
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-05-21 23:10:51 -0700

    chore: start each bench with own server

--- loglogd/benches/basic-bench.rs
+++ loglogd/benches/basic-bench.rs
@@ -7,18 +7,17 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use loglog::std::Client;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let server = TestLoglogd::new().unwrap();
-
     let entries = [
         [1u8].as_slice(),
         [2u8; 1000].as_slice(),
         [3u8; 100000].as_slice(),
     ];
 
-    let mut client = server.new_client().unwrap();
     {
         let mut write_nocommit = c.benchmark_group("write nocommit");
         for entry in entries {
+            let server = TestLoglogd::new().unwrap();
+            let mut client = server.new_client().unwrap();
             write_nocommit.throughput(criterion::Throughput::Bytes(u64::expect_from(entry.len())));
             write_nocommit.bench_with_input(
                 BenchmarkId::from_parameter(entry.len()),
@@ -31,6 +30,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     {
         let mut write_commit = c.benchmark_group("write commit");
         for entry in entries {
+            let server = TestLoglogd::new().unwrap();
+            let mut client = server.new_client().unwrap();
             write_commit.throughput(criterion::Throughput::Bytes(u64::expect_from(entry.len())));
             write_commit.bench_with_input(
                 BenchmarkId::from_parameter(entry.len()),
@@ -41,10 +42,10 @@ fn criterion_benchmark(c: &mut Criterion) {
         write_commit.finish();
     }
     {
-        // new client to ignore the previous entries
-        let mut client = server.new_client().unwrap();
         let mut roundtrip = c.benchmark_group("roundtrip");
         for entry in entries {
+            let server = TestLoglogd::new().unwrap();
+            let mut client = server.new_client().unwrap();
             roundtrip.throughput(criterion::Throughput::Bytes(u64::expect_from(entry.len())));
             roundtrip.bench_with_input(
                 BenchmarkId::from_parameter(entry.len()),
@@ -60,8 +61,31 @@ fn criterion_benchmark(c: &mut Criterion) {
         }
         roundtrip.finish();
     }
+    {
+        // new client to ignore the previous entries
+        let mut roundtrip = c.benchmark_group("roundtrip-behind");
 
-    drop(client);
+        for entry in entries {
+            let server = TestLoglogd::new().unwrap();
+            let mut client = server.new_client().unwrap();
+            for _ in 0..1000 {
+                client.append_nocommit(entry).unwrap();
+            }
+            roundtrip.throughput(criterion::Throughput::Bytes(u64::expect_from(entry.len())));
+            roundtrip.bench_with_input(
+                BenchmarkId::from_parameter(entry.len()),
+                entry,
+                |b, entry| {
+                    b.iter(|| {
+                        client.append_nocommit(entry).unwrap();
+                        let read = client.read().unwrap();
+                        assert_eq!(entry.len(), read.len());
+                    })
+                },
+            );
+        }
+        roundtrip.finish();
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
